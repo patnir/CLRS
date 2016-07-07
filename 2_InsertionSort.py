9ef86fb78af7b427544bfc66715cad3cb285866a
patnir <rahul20patni@gmail.com>
2016-07-06 20:24:32 -0400

    Insertion sort and pseudocode conventions

--- 2_InsertionSort.py
+++ 2_InsertionSort.py
@@ -14,12 +14,23 @@ def insertionSort(A):
             A[i + 1] = A[i]
             i = i - 1
         A[i + 1] = key
-    return A
     
 
 
-A = [31, 41, 59, 26, 41, 58]
+A = [5, 4, 3, 2]
 
-A = insertionSort(A)
-
+def insertionSort2(A):
+    for j in range(1, len(A)):
+        key = A[j]
+        i = j - 1
+        while i > -1 and A[i] > key:
+            A[i + 1] = A[i]
+            i = i - 1
+        A[i + 1] = key
+        
 print A
+
+insertionSort2(A)
+print range(1, len(A))
+
+print A    
