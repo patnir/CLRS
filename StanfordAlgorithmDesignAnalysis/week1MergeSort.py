08a81fda012b2c711cd6021f6945446c535f7046
patnir <rahul20patni@gmail.com>
2016-07-10 11:13:08 -0400

    Merge sort implementation

--- StanfordAlgorithmDesignAnalysis/week1MergeSort.py
+++ StanfordAlgorithmDesignAnalysis/week1MergeSort.py
@@ -8,13 +8,12 @@ Created on Wed Jul 06 20:25:57 2016
 
 
 # Merging and returning two sorted arrays
-def Merge(A, B):
+def MergeTwoArrays(A, B):
     n = len(A) + len(B)
     mergedArray = [None] * n
     i = 0
     j = 0    
     for k in range(n):
-        print i, j
         if j == len(B) or (i < len(A) and A[i] < B[j]):
             mergedArray[k] = A[i] 
             i += 1
@@ -23,4 +22,37 @@ def Merge(A, B):
             j += 1
     return mergedArray
 
-print Merge([2, 3, 6, 7, 9], [1, 4, 5, 8])
+def Merge(A, p, q, r):
+    B = [None] * (q - p + 1)
+    C = [None] * (r - q)
+    k = 0
+    for i in range(p, q + 1):
+        B[k] = A[i]
+        k += 1
+    k = 0
+    for i in range(q + 1, r + 1):
+        C[k] = A[i]
+        k += 1
+    i = 0
+    j = 0
+    for k in range(p, r + 1):
+        if j == len(C) or (i < len(B) and B[i] < C[j]):
+            A[k] = B[i] 
+            i += 1
+        else:
+            A[k] = C[j]
+            j += 1
+    return A
+
+def MergeSort(A, p, r):
+    if p < r:
+        q = int((p + r) / 2)
+        MergeSort(A, p, q)
+        MergeSort(A, q + 1, r)
+        Merge(A, p, q, r)
+    return A
+
+print MergeTwoArrays([1, 4, 5, 8], [2, 3, 6, 7, 9])
+print Merge([100, 6, 7, 8, 9, 1, 2, 3, 4, 5, 120], 1, 4, 9)
+A = [1, 4, 5, 2, 3, 6, 7, 8, 9]
+print MergeSort(A, 0, len(A) - 1)
