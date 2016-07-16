0a7496aabf331db6fb2278437339731b1435aa3c
patnir <rahul20patni@gmail.com>
2016-07-16 15:36:23 -0400

    Closest pair set-up

---
+++ StanfordAlgorithmDesignAnalysis/week1ClosestPair.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 12 20:59:10 2016
+
+@author: Rahul Patni
+"""
+
+# Algorithm for finding the closest pair of points on 2D plane
+
+P = [[], [], [], [], [], []]
