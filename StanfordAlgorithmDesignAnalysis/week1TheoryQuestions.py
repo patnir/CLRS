e5ba6a6ff6c9788f4ec76b4492467d63b143fd4c
patnir <rahul20patni@gmail.com>
2016-07-16 15:36:53 -0400

    Theory questions for week 1

---
+++ StanfordAlgorithmDesignAnalysis/week1TheoryQuestions.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 13 21:59:05 2016
+
+@author: Rahul Patni
+#"""
+
+#You are given as input an unsorted array of n distinct numbers, where n is a 
+#power of 2. Give an algorithm that identifies the second-largest number 
+#in the array, and that uses at most n+log2n−2 comparisons.
+
+#You are a given a unimodal array of n distinct elements, meaning that its 
+#entries are in increasing order up until its maximum element, after which 
+#its elements are in decreasing order. Give an algorithm to compute the maximum 
+#element that runs in O(log n) time.
+
+#You are given a sorted (from smallest to largest) array A of n distinct 
+#integers which can be positive, negative, or zero. You want to decide whether 
+#or not there is an index i such that A[i] = i. Design the fastest algorithm 
+#that you can for solving this problem.
