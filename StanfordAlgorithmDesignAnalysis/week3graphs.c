c4e81c69ccb2ad4f2db8283fb3ea2b3a7d435b6d
rpatni <rpatni@purdue.edu>
2016-07-31 18:12:21 -0400

    implementing graphs in C

--- StanfordAlgorithmDesignAnalysis/week3graphs.c
+++ StanfordAlgorithmDesignAnalysis/week3graphs.c
@@ -2,22 +2,36 @@
 #include<stdlib.h>
 
 typedef struct _node{
-	int n;
+	int identity;
+	int totalLinks;
 	struct _node *link;
 } node;
 
+typedef struct _graph {
+	node *nodes;
+} graph;
 
 node *node_construct(int n)
 {
 	node *new_node = (node *)malloc(sizeof(node));
-	new_node->n = n;
+	new_node->identity = n;
+	new_node->totalLinks = 0;
 	return new_node;
+}
+
+graph *graph_construct()
+{
+	graph *new_graph = (graph *)malloc(sizeof(graph));
+	int i = 0;
+	for (i = 0; i < 200; i++) {
+		new_graph->nodes[i] = node_construct(i + 1);
+	}
+	return new_graph;
 }
 
 int main(int argc, char** argv)
 {
-	node *firstNode = NULL;
-	firstNode = node_construct(5);
-	printf("firstNode->n = %d\n", firstNode->n);
+	graph *graph1 = graph_construct();
+	printf("%d\n", (graph1->nodes[1]).identity);
 	return EXIT_SUCCESS;
 }
