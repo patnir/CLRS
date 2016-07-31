02812384a8867a22cfd28ea3e3a19c4c97c4c15f
patnir <rahul20patni@gmail.com>
2016-07-31 10:14:22 -0400

    replace and delete key implementation

--- StanfordAlgorithmDesignAnalysis/week3Graphs.py
+++ StanfordAlgorithmDesignAnalysis/week3Graphs.py
@@ -7,6 +7,8 @@ Created on Sun Jul 31 00:21:57 2016
 
 # week 3 Graphs
 
+import random
+
 def initializeGraph():
     graph = dict()
     for i in range(1, 201):
@@ -19,17 +21,45 @@ def loadGraph(graph):
     for line in fptr:
         i = line.split("\t")
         key = int(i[0])
-        print "key", key
         for x in range(1, len(i)):
             if i[x] != "\n":
                 graph[key].append(int(i[x]))
-            
+
+def randomizedContraction(graph):
+    i = 0
+    while len(graph) > 2:
+        x = random.randint(1, 200)
+        if x in graph:
+            y = random.randint(0, len(graph[x]) - 1)
+            z = graph[x][y]
+            print "z", z
+            print "x", x
+            delKey(graph, x)
+            replaceKey(graph, z, x)
+            i += 1
+        else:
+            continue
+    return
+
+def replaceKey(graph, toReplace, node):
+    for i in range(1, len(graph) + 1):
+        if i in graph:
+            if toReplace in graph[i]:
+                indexToReplace = graph[i].index(toReplace)
+                graph[i][indexToReplace] = node
+
+def delKey(graph, key):
+    del graph[key]
 
 def main():
     graph = initializeGraph()
-    print graph
     loadGraph(graph)
+    randomizedContraction(graph)
     print graph
+    if graph.keys()[0] in graph[graph.keys()[1]]:
+        print "yes"
+    if graph.keys()[1] in graph[graph.keys()[0]]:
+        print "yes2"
     return
     
 if __name__ == "__main__":
