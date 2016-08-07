178e2216622bb8e1b5e169a189de63c444e01bab
patnir <rahul20patni@gmail.com>
2016-08-07 14:34:08 -0400

    Adding queue and appending false to indicate explored or not

--- StanfordAlgorithmDesignAnalysis/week4BreadthFirstSearchShortestPath.py
+++ StanfordAlgorithmDesignAnalysis/week4BreadthFirstSearchShortestPath.py
@@ -7,6 +7,27 @@ Created on Sun Aug 07 12:46:13 2016
 
 # Bread-first search shortest path
 
+class Queue():
+    def __init__(self):
+        self.head = 0
+        self.tail = 0
+        self.size = 0
+        self.data = []
+        
+    def empty(self):
+        return len(self.data) == 0
+    
+    def enqueue(self, c):
+        self.data.append(c)
+        self.tail += 1
+        self.size += 1
+        
+    def dequeue(self):
+        x = self.data[self.head]
+        self.size -= 1
+        self.head += 1
+        return x
+
 def loadGraph():
     graph = dict()
     filename = "Graph1BFSShortestPath.txt"
@@ -15,18 +36,28 @@ def loadGraph():
         i = line.split("\t")
         graph[i[0]] = []
         for x in range(1, len(i)):
+            i[x] = i[x].rstrip()
             if i[x] != "\n":
-                i[x] = i[x].rstrip()
+                if i[x] not in graph:
+                    graph[i[x]] = []
                 graph[i[0]].append(i[x])
+        graph[i[0]].append(False)
     return graph
                 
 def printGraph(graph):
     keys = graph.keys()
     for i in keys:
+        if len(graph[i]) == 0:
+            graph[i].append(False)
         print i, graph[i]
     return
     
 def BFS(graph, start, target):
+    q = Queue()
+    q.enqueue(graph[start])
+    while q.size != 0:
+        node = q.dequeue()
+        print node
     return
           
 def main():
