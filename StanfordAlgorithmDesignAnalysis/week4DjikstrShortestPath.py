8f4c7b6e44fa950273bbd777634ab276db2a25d3
patnir <rahul20patni@gmail.com>
2016-08-10 14:56:45 -0400

    Shortest path implementation with arrays O(mn) time

--- StanfordAlgorithmDesignAnalysis/week4DjikstrShortestPath.py
+++ StanfordAlgorithmDesignAnalysis/week4DjikstrShortestPath.py
@@ -31,6 +31,32 @@ def dijkstra(graph, start):
     A[start] = 0
     B = dict()
     B[start] = []
+    while len(X) != len(graph):
+        routes = []
+        length = []
+        source = []
+        for i in X:
+            
+            for j in graph[i]:
+                if j[0] not in X:
+                    if j[0] not in B:
+                        B[j[0]] = []
+                    source.append(i)
+                    routes.append(j[0])
+                    lvw = j[1]
+                    length.append(A[i] + lvw)
+        shortest = length.index(min(length))
+        X.append(routes[shortest])
+        A[routes[shortest]] = min(length)
+        new_list = list(B[source[shortest]])
+        B[routes[shortest]] = new_list
+        B[routes[shortest]].append(routes[shortest])
+    print "X"
+    print X
+    print "A"
+    print A
+    print "B"
+    print B
     return
 
 def main():
