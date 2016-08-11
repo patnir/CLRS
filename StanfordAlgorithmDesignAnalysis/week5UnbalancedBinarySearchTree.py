ed639c37b6e943ee5e65b3ffbefe1016ec312ce4
patnir <rahul20patni@gmail.com>
2016-08-11 06:07:26 -0400

    Initial all required functions

---
+++ StanfordAlgorithmDesignAnalysis/week5UnbalancedBinarySearchTree.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 11 06:04:03 2016
+
+@author: Rahul Patni
+"""
+
+# Unbalanced Binary Search Tree
+
+class BST():
+    def __init__(self):
+        self.size = 0
+        
+    def insert(self, number):
+        return
+        
+    def search(self, number):
+        return
+        
+    def postOrder(self):
+        return
+        
+    def preOrder(self):
+        return
+        
+    def inOrder(self):
+        return
+        
+
+        
