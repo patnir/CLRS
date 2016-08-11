6a1987ca326fdfb5633599b2732cd52126b7a2a1
patnir <rahul20patni@gmail.com>
2016-08-10 21:08:09 -0400

    Programming assignment data and solution

--- StanfordAlgorithmDesignAnalysis/week5DjikstrShortestPath.py
+++ StanfordAlgorithmDesignAnalysis/week5DjikstrShortestPath.py
@@ -11,20 +11,23 @@ import sys
 
 def loadGraph():
     graph = dict()
-    filename = "Graph5Dijkstra.txt"
+    filename = "dijkstraData.txt"
+    #filename = "Graph5Dijkstra.txt"
     fptr = open(filename)
     for line in fptr:
-        line = line.rstrip().split(" ")
+        line = line.rstrip().split("\t")
         if line[0] not in graph:
             graph[line[0]] = []
-        if line[1] not in graph:
-            graph[line[1]] = []
-        graph[line[0]].append([line[1], int(line[2])])
+        for i in range(1, len(line)):
+            location = line[i].split(",")
+            if location[0] not in graph:
+                graph[location[0]] = []
+            graph[line[0]].append([location[0], int(location[1])])
     return graph
     
 def printGraph(graph):
     for i in graph.keys():
-        print i, graph[i]
+        print i
     
 def dijkstra(graph, start):
     # Initializing components
@@ -42,7 +45,7 @@ def dijkstra(graph, start):
                 if j[0] not in X:
                     if j[0] not in B:
                         B[j[0]] = []
-                    if j[1] < minimumLength:
+                    if A[i] + j[1] < minimumLength:
                         minimumLength = A[i] + j[1]
                         minimumSource = i
                         minimumRoute = j[0]
@@ -51,18 +54,19 @@ def dijkstra(graph, start):
         new_list = list(B[minimumSource])
         B[minimumRoute] = new_list
         B[minimumRoute].append(minimumRoute)
-    print "X"
-    print X
-    print "A"
-    print A
-    print "B"
-    print B
-    return
+    return A
 
 def main():
     graph = loadGraph()
     printGraph(graph)
-    dijkstra(graph, 's')
+    A = dijkstra(graph, '1')
+    print A
+    array = ['t','w','7','37','59','82','99','115','133','165','188','197']
+    for i in array:
+        if i not in A:
+            print "weirds", i
+        else:
+            print A[i], 
     return
     
 if __name__ == "__main__":
