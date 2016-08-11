5bd1b6d97f87228dd87964c5d7f039baa36ca5c2
patnir <rahul20patni@gmail.com>
2016-08-11 14:57:10 -0400

    Delete implementation attempt

--- StanfordAlgorithmDesignAnalysis/week5UnbalancedBinarySearchTree.py
+++ StanfordAlgorithmDesignAnalysis/week5UnbalancedBinarySearchTree.py
@@ -70,6 +70,25 @@ class Node():
         if self.right != None:        
             self.right.inOrder()
         return
+        
+    def goLeft(self):
+        if self.left == None:
+            return self.value
+        else:
+            return self.left.goLeft()
+            
+    def goRight(self):
+        if self.right == None:
+            return self.value
+        else:
+            return self.right.goRight()
+
+           
+    def pred(self, key, val):
+        return key
+    
+    def succ(self):
+        return
 
 class BST():
     def __init__(self):
@@ -111,18 +130,82 @@ class BST():
             self.root.inOrder()
             print "\n"
         return
+    
+    def minElement(self):
+        if self.root == None:
+            return None
+        else:
+            return self.root.goLeft()
+            
+    def maxElement(self):
+        if self.root == None:
+            return None
+        else:
+            return self.root.goRight()
+            
+    def pred(self, val):
+        if self.root == None:
+            return None
+        return self.root.pred(None, val)
+            
+    def succ(self, val):
+        if self.root == None:
+            return None
+        return self.root.succ(val)
+        
+    def delete(self, val):
+        if self.root == None:
+            return
+        prev = None
+        curr = self.root
+        while curr != None:
+            if val == curr.value:
+                break
+            prev = curr
+            if val > curr.value:
+                curr = curr.right
+            else:
+                curr = curr.left
+        if curr == None:
+            return False
+        if curr.left == None and curr.right == None:
+            prev = None
+        elif curr.left == None:
+            if curr.value < prev.value:
+                prev.left = curr.right
+            else:
+                prev.right = curr.right
+        elif curr.right == None:
+            if curr.value < prev.value:
+                prev.left = curr.left
+            else:
+                prev.right = curr.left
+        else:
+            pred = curr.left
+            while pred.right != None:
+                pred = pred.right
+            pred.right = None
+            curr.value = pred.value
+        return True
         
 def main():
     array = []
-    for i in range(0, 10):
-        array.append(random.randint(0, 100))
+    for i in range(0, 2):
+        number = random.randint(0, 100)
+        array.append(number)
+        if i == 9:
+            key = number
     print array
     tree = BST()
     for i in array:
         tree.insert(i)
-    tree.inOrder()
     tree.preOrder()
-    tree.postOrder()
-    
+    tree.inOrder()
+    #key = tree.maxElement()
+    key = tree.minElement()
+    print key
+    print "key", tree.delete(key), key
+    tree.inOrder()
+
 if __name__ == "__main__":
     main()
