940865de62f43d1bca22f58fd179753cfc632f4f
patnir <rahul20patni@gmail.com>
2016-08-11 06:23:50 -0400

    insert and search implementation

--- StanfordAlgorithmDesignAnalysis/week5UnbalancedBinarySearchTree.py
+++ StanfordAlgorithmDesignAnalysis/week5UnbalancedBinarySearchTree.py
@@ -7,17 +7,70 @@ Created on Thu Aug 11 06:04:03 2016
 
 # Unbalanced Binary Search Tree
 
-class BST():
-    def __init__(self):
-        self.size = 0
+class Node():
+    def __init__(self, val):
+        self.value = val
+        self.left = None
+        self.right = None
         
-    def insert(self, number):
-        return
+    def insert(self, val):
+        if val <= self.value:
+            if self.left == None:
+                self.left = Node(val)
+            else:
+                self.left.insert(val)
+        else:
+            if self.right == None:
+                self.right = Node(val)
+            else:
+                self.right.insert(val)
         
-    def search(self, number):
+    def search(self, val):
+        if val == self.value:
+            return True
+        if val < self.value:
+            if self.left == None:
+                return False
+            else:
+                return self.left.search(val)
+        else:
+            if self.right == None:
+                return False
+            else:
+                return self.right.search(val)
         return
         
     def postOrder(self):
+        return 
+        
+    def preOrder(self):
+        return
+        
+    def inOrder(self):
+        return
+
+class BST():
+    def __init__(self):
+        self.root = None
+        
+    def insert(self, val):
+        if self.root != None:
+            self.root.insert(val)
+            return
+        else:
+            self.root = Node(val)
+        return
+        
+    def search(self, val):
+        if self.root == None:
+            return False
+        return self.root.search(val)
+        
+    def postOrder(self):
+        if self.root == None:
+            return None
+        else:
+            print self.root.postOrder()
         return
         
     def preOrder(self):
