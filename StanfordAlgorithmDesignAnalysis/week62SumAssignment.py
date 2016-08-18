5d5c8a43139147f9fddabff9cd6c0a93e0ba76d2
patnir <rahul20patni@gmail.com>
2016-08-18 00:20:20 -0400

    Testing on smaller scale

--- StanfordAlgorithmDesignAnalysis/week62SumAssignment.py
+++ StanfordAlgorithmDesignAnalysis/week62SumAssignment.py
@@ -8,6 +8,8 @@ Created on Wed Aug 17 18:24:19 2016
 # 2 SUM Week 6 Assignment
 #import array
 
+import random
+
 def loadData():
     filename = "2sum.txt"
     fptr = open(filename)
@@ -52,16 +54,50 @@ def TwoSum(A):
         start = i
         end = start + 1
         diff = []
-        t = abs(A[start] - A[end])
-        while t <= 10000:
-            if t not in diff:
+        if A[start] > 10000:
+            break
+        if A[start] == A[end]:
+            continue
+        t = abs(A[start] + A[end])
+        while end < len(A) - 1:
+            if t not in diff and t <= 10000:
                 diff.append(t)
             end += 1
-            if end == len(A):
-                break
-            t = abs(A[start] - A[end])
+            t = abs(A[start] + A[end])
         total += len(diff)
     return total
+    
+def printArray(A):
+    for i in range(5000, len(A)):
+        print A[i],
+        if i > 30:
+            return
+
+def testing():
+#    A = []
+#    for i in range(20):
+#        A.append(random.randint(0, 20))
+#    QuickSort(A, 0, len(A) - 1)
+    A = [0, 1, 1, 1, 2, 2, 5, 5, 9, 10, 10, 10, 11, 11, 13, 13]# 12, 13, 13, 14, 15, 16, 16, 17, 18, 20, 20, 20]
+    total = 0
+    for i in range(len(A) - 1):
+        print "for", A[i]
+        start = i
+        end = start + 1
+        diff = []
+        if A[start] >= 3:
+            break
+        if A[start] == A[end]:
+            continue
+        t = abs(A[start] + A[end])
+        while end < len(A) - 1:
+            if t not in diff and t < 3:
+                diff.append(t)
+                print "end", A[end]
+            end += 1
+            t = abs(A[start] + A[end])
+        total += len(diff)
+    print total
 
 def main():
     A = loadData()
@@ -69,6 +105,9 @@ def main():
     QuickSort(A, 0, len(A) - 1)
     print "sort complete"
     print TwoSum(A)
+    #print "now printing"
+    #printArray(A)
+    #testing()
     
     
 if __name__ == "__main__":
