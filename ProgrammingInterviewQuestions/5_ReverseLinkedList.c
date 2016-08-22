78488ebb00ba023c2cef5303ca5920c26c1ea1b7
patnir <rahul20patni@gmail.com>
2016-08-21 22:22:51 -0400

    Set up

---
+++ ProgrammingInterviewQuestions/5_ReverseLinkedList.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node{
+	int data;
+	struct Node* next;
+}Node;
+
+void reverse(Node **head) {
+	return;
+}
+
+int main(int argc, char**argv)
+{
+	Node *head = NULL;
+	return EXIT_SUCCESS;
+}
