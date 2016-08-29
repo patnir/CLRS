3cce09f3c46418bb0fc6dfa685ea6ca70e64a9f7
patnir <rahul20patni@gmail.com>
2016-08-28 20:23:54 -0400

    BST Delete implementation

--- ProgrammingInterviewQuestions/12_BinaryTreeIsBST.py
+++ ProgrammingInterviewQuestions/12_BinaryTreeIsBST.py
@@ -23,6 +23,22 @@ class Node:
             self.right.inorder()
         return
         
+    def preorder(self):
+        print self.value,
+        if self.left != None:
+            self.left.preorder()
+        if self.right != None:
+            self.right.preorder()
+        return        
+        
+    def inOrderBST(self, array):     
+        if self.left != None:
+            self.left.inOrderBST(array)
+        array.append(self.value)
+        if self.right != None:
+            self.right.inOrderBST(array)
+        return
+        
 class Tree:
     def __init__(self):
         self.head = None
@@ -52,13 +68,98 @@ class Tree:
             return 
         self.head.inorder()
         
+    def preOrder(self):
+        if self.head == None:
+            print "Empty"
+            return 
+        self.head.preorder()
+        
+    def checkBST(self):
+        if self.head == None:
+            return True
+        A = []
+        self.head.inOrderBST(A)
+        return A 
+        
+def delete(tree, val):
+    if tree.head == None:
+        print "empty"
+        return False
+    curr = tree.head
+    prev = None
+    while curr != None and curr.value != val:
+        prev = curr
+        if curr.value > val:
+            curr = curr.left
+        else:
+            curr = curr.right 
+    if curr == None:
+        print "not found"
+        return False
+    print "found"
+    if curr.left == None and curr.right == None:
+        print "both none"
+        if prev == None:
+            tree.head = None
+        elif curr.value < prev.value:
+            prev.left = None
+        else:
+            prev.right = None
+    elif curr.left != None and curr.right == None:            
+        print "right is none"
+        if prev == None:
+            tree.head = curr.left
+        elif curr.value < prev.value:
+            prev.left = curr.left
+        else:
+            prev.right = curr.left
+    elif curr.right != None and curr.left == None:
+        print "left is none"
+        if prev == None:
+            tree.head = curr.right
+        elif curr.value < prev.value:
+            prev.left = curr.right
+        else:
+            prev.right = curr.right
+    else:
+        print "both are present"
+        toRepPrev = None
+        toRep = curr.left
+        while toRep.right != None:
+            toRepPrev = toRep
+            toRep = toRep.right
+        if toRepPrev != None:
+            toRepPrev.right = toRep.left
+        else:
+            curr.left = toRep.left
+        toRep.left = curr.left
+        toRep.right = curr.right
+        curr.left = None
+        curr.right = None
+        if prev != None:
+            if curr.value < prev.value:
+                prev.left = toRep
+            else:
+                prev.right = toRep
+        else:
+            tree.head = toRep
+    return True
+
 def main():
     tree = Tree()
-    for i in range(0, 10):
-        value = random.randint(0, 100)
+    val = 0
+    for i in range(0, 14):
+        value = random.randint(20, 99)
         print "inserting", value
+        if i == 7:
+            val = value
         tree.insert(value)
     tree.inOrder()
+    print "finding", val
+    print delete(tree, val)
+    tree.inOrder()
+    print
+    tree.preOrder()
     return
     
 main()
