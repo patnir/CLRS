e7dc65d4e17f13878a514507e590e85e0721d15a
patnir <rahul20patni@gmail.com>
2016-09-17 20:23:46 -0400

    Set up

---
+++ ProgrammingInterviewQuestions/22_ValidNParenthesis.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 17 15:36:48 2016
+
+@author: Rahul Patni
+"""
+
+# all valid combinations of n - pairs of parenthesis
+
+gOpen = "("
+gClose = ")"
+
+def main():
+    global gOpen, gClose
+    print gOpen + gClose
+    
+main()
