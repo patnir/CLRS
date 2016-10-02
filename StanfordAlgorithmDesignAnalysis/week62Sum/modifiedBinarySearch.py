be32c2773ccc03c9eb79c1468c7c757223cbeefd
patnir <rahul20patni@gmail.com>
2016-10-01 21:12:14 -0400

    binary seach to find the target or the closest number

---
+++ StanfordAlgorithmDesignAnalysis/week62Sum/modifiedBinarySearch.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 01 21:00:45 2016
+
+@author: Rahul Patni
+"""
+
+# modifies binary
+
+def modifiedBinarySearch(A, target, start, end):
+    while start <= end:
+        mid = (start + end) / 2
+        print A[mid]
+        if A[mid] == target:
+            return mid
+        if A[mid] > target and A[mid - 1] < target:
+            return mid
+        if A[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return -1
+    
+    
+def main():
+    A = [1, 2, 3, 4, 11]
+    print modifiedBinarySearch(A, 15, 0, len(A) - 1)
+    
+    
+main()
