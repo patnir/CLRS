ef2eba47481d91e7c46ebbab0d536a4781a7d825
patnir <rahul20patni@gmail.com>
2016-10-10 08:17:46 -0400

    Fix

--- ProgrammingInterviewQuestions/39_StackTreeTraversal.py
+++ ProgrammingInterviewQuestions/39_StackTreeTraversal.py
@@ -13,26 +13,6 @@ self.left (the left child of the node)
 self.right (the right child of the node)
 self.data (thevalue of the node)
 """
-
-class Node():
-    def __init__(self, val):
-        self.val = val
-        self.n = None
-
-class Stack():
-    def __init__(self):
-        self.head = None
-    
-    def push(self, val):
-        node = Node(val)
-        if self.head == None:
-            self.head = node
-            return 
-        node.n = self.head
-        self.head = node
-        return
-    
-    def pop(self):# -*- coding: utf-8 -*-
 """
 Created on Sat Oct 08 19:41:47 2016
 
