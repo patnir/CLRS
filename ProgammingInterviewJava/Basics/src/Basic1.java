78894a4d5e32a33eed5b29b3631e02f91b2c0e90
patnir <rahul20patni@gmail.com>
2016-10-30 19:22:49 -0400

    removing java directory

--- ProgammingInterviewJava/Basics/src/Basic1.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Rahul Patni on 9/19/2016.
- */
-public class Basic1 {
-    public static void main(String[] arguments) {
-        System.out.println("What is this that I am doing?");
-    }
-}
